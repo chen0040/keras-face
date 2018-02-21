57b9e598d3bb07de3e1985259791b3cb39099de1
xschen <xs0040@gmail.com>
2018-02-21 16:20:42 +0800

    some opencv face detection code

--- keras_face/library/opencv_utils.py
+++ keras_face/library/opencv_utils.py
@@ -1,16 +1,19 @@
 import numpy as np
 import cv2
+import os
 
 
 def detect_face_from_img_path(frontal_face_model_file_path, image_path):
+    if not os.path.exists(frontal_face_model_file_path):
+        print('failed to find face detection opencv model: ', frontal_face_model_file_path)
+
     face_cascade = cv2.CascadeClassifier(frontal_face_model_file_path)
 
     img = cv2.imread(image_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    print(gray)
-
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    print('faces detected: ', len(faces))
     for (x, y, w, h) in faces:
         img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
@@ -24,7 +27,7 @@ def main():
 
     detect_face_from_img_path(
         frontal_face_model_file_path,
-        '../../demo/data/images/camera_3.jpg')
+        '../../demo/data/opencv-images/test1.jpg')
 
 
 if __name__ == '__main__':
